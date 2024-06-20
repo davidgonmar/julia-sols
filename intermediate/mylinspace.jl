e1103a3e9911f36688f03c67569627ae6bd92f80
David González Martínez <me@davidgonmar.me>
2024-06-21 00:03:09 +0200

    add mylinspace compat with Unitful

--- intermediate/mylinspace.jl
+++ intermediate/mylinspace.jl
@@ -1,21 +1,22 @@
 using Test
+using Unitful
 
 
-struct MyLinspace
-	start::Float64
-	stop::Float64
+struct MyLinspace{T}
+	start::T
+	stop::T
 	N::Int
 end
 
-function mylinspace(start::Float64, stop::Float64; length::Int = 50)
+function mylinspace(start::T, stop::T; length::Int = 50) where {T}
 	if length < 2
 		throw(ArgumentError("length must be at least 2"))
 	end
 	MyLinspace(start, stop, length)
 end
 
-
 function _getval(r::MyLinspace, i::Int)
+	T = eltype(r.start)
 	(1 <= i <= r.N) || throw(BoundsError(r, i))
 	r.start + (i - 1) / (r.N - 1) * (r.stop - r.start)
 end
@@ -31,14 +32,18 @@ Base.length(r::MyLinspace) = r.N
 Base.getindex(r::MyLinspace, i::Int) = _getval(r, i)
 
 
+# define zero{Quantity{Float64}} so tests do not complain
+Base.zero(::Type{Quantity{Float64}}) = 0.0u"m"
+
+
 macro test_mylinspace(start, stop, N, indices_to_try)
 	return quote
 		r = mylinspace($(esc(start)), $(esc(stop)), length = $(esc(N)))
 		rr = range($(esc(start)), $(esc(stop)), length = $(esc(N)))
 		@test length(r) == length(rr)
-		@test collect(r) ≈ collect(rr)
+		@test collect(r) == collect(rr)
 		for i in $indices_to_try
-			@test r[i] ≈ rr[i]
+			@test r[i] == rr[i]
 		end
 	end
 end
@@ -47,5 +52,9 @@ end
 	@test_mylinspace 1.0 5.0 5 [1, 2, 3, 4, 5]
 	@test_mylinspace 1.0 5.0 3 [1, 2, 3]
 	@test_mylinspace 5.0 1.0 5 [1, 2, 3, 4, 5]
+
+	@test_mylinspace 1.0u"m" 5.0u"m" 5 [1, 2, 3, 4, 5]
+
+	@test_mylinspace 1.0u"m" 5.0u"m" 3 [1, 2, 3]
 end
 
